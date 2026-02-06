06a4da735cb07862a3948d867bdac1cb0f602d1c
Kirill Klemazov <klemazov_kirill@mail.ru>
2026-02-06 19:04:53 +0300

    some bad code

--- src/reaction_constant_model.jl
+++ src/reaction_constant_model.jl
@@ -1,13 +1,26 @@
 
-struct ReactionRateA end
+abstract type ReactionRateModel end
 
-struct ReactionRateB end
 
-struct ReactionRateC end
+function K(reaction_rate::ReactionRateModel)
+    A = reaction_rate.A
+    Ea = reaction_rate.Ea
+    T = reaction_rate.T
+    return A*exp(-Ea/(R*T))
+end
 
-K(A, Ea,T) = A*exp(-Ea/(R*T))
-K(A, Ea,T, ::Type{ReactionRateA}) = K(A, Ea,T)
+struct ReactionRate{T} <: ReactionRateModel
+    A::T
+    Ea::T
+    T::T
+end
 
-K(B,Ea,T,::Type{ReactionRateB}) = B*√T*exp(-Ea/(R*T))
-K(C, Ea, T, ::Type{ReactionRateC}) = C*T*exp(-Ea/(R*T))
+Ka = ReactionRate(1e5,1e5,1000.0)
+
+K(Ka)
+# K(A, Ea,T) = A*exp(-Ea/(R*T))
+# K(A, Ea,T, ::Type{ReactionRateA}) = K(A, Ea,T)
+
+# K(B,Ea,T,::Type{ReactionRateB}) = B*√T*exp(-Ea/(R*T))
+# K(C, Ea, T, ::Type{ReactionRateC}) = C*T*exp(-Ea/(R*T))
 
